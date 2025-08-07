b40f2844c00d3de0dbcf902cef54a385cabbe252
kenth356 <kjsamonte27@gmail.com>
2025-08-07 20:16:24 +0800

    some updates, might be the final unless i get the motivation to continue the formatting the UI

--- login.java
+++ login.java
@@ -1,34 +1,26 @@
-// Log-in System in Java
-// Mariano, Kenth Jarren S.
-
 import java.util.*;
 import java.io.*;
 
-class UserDC {
-    String name;
-    String passw;
-
-    public UserDC(String name, String passw) {
-        this.name = name;
-        this.passw = passw;
-    }
+class usersDC {
+    String name, pass;
 }
 
 public class login {
-    static List<UserDC> usersD = new ArrayList<>();
+    static Scanner input = new Scanner(System.in);
     static String REGISname = "";
-    static Scanner scanner = new Scanner(System.in);
+    static List<usersDC> usersD = new ArrayList<>();
     public static void main(String[] args) {
-        int choice;
         while (true) {
-            System.out.println("\n\n" + String.format("%75s", "================================"));
-            System.out.println(String.format("%66s", "[   WELCOME   ]"));
-            System.out.println(String.format("%75s", "================================"));
-            System.out.println("\n" + String.format("%86s", "1. Registration           2. Log in          3. Exit"));
-            System.out.println(String.format("%89s", "----------------------------------------------------------"));
-            System.out.print("\n\n" + String.format("%39s", "Enter: "));
+            int choice;
+            System.out.println("\n================");
+            System.out.println("\n[   WELCOME   ]");
+            System.out.println("\n================");
+            System.out.println("\n1. Registration");
+            System.out.println("\n2. Login");
+            System.out.println("\n3. Exit");
+            System.out.print("\nEnter: ");
             try {
-                choice = Integer.parseInt(scanner.nextLine());
+                choice = Integer.parseInt(input.nextLine());
             } catch (NumberFormatException e) {
                 choice = -1;
             }
@@ -40,120 +32,80 @@ public class login {
                 LOGIN();
                 break;
                 case 3:
-                EXIT();
+                exit();
                 return;
                 default:
-                System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-                System.out.println("\n" + String.format("%76s", "[  PLEASE ENTER A CORRECT INPUT  ]"));
-                System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("\n[PLEASE ENTER A VALID INPUT]");
+                }
             }
         }
     }
 
     public static void REGIS() {
-        String REGname, REGpassw;
-        System.out.println("\n\n" + String.format("%75s", "==============================="));
-        System.out.println(String.format("%68s", "[  REGISTRATION  ]"));
-        System.out.println(String.format("%75s", "==============================="));
-        System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-        System.out.print("\n" + String.format("%54s", "Enter your full name: "));
-        REGname = scanner.nextLine();
-        System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-        System.out.print("\n" + String.format("%54s", "Create your password: "));
-        REGpassw = scanner.nextLine();
-        usersD.add(new UserDC(REGname, REGpassw));
+        usersDC users = new usersDC();
+        System.out.println("\n[REGISTRATION]");
+        System.out.print("\nEnter your full name: ");
+        users.name = input.nextLine();
+        System.out.print("\nCreate your password: ");
+        users.pass = input.nextLine();
+        usersD.add(users);
         SAVEDFILES();
-        System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-        System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-        System.out.println(String.format("%74s", "[  REGISTRATION SUCCESSFUL  ]"));
-        System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
+        System.out.println("\n[REGISTRATION SUCCESSFUL]");
     }
 
     public static void LOGIN() {
         while (true) {
             String LOGINname, LOGINpass;
-            boolean USERHANDLING, PASSHANDLING;
-            LOGINname = "";
-            LOGINpass = "";
-            USERHANDLING = false;
-            PASSHANDLING = false;
-            System.out.println("\n\n" + String.format("%75s", "==============================="));
-            System.out.println(String.format("%66s", "[  LOG - IN  ]"));
-            System.out.println(String.format("%75s", "==============================="));
-            System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-            System.out.print("\n" + String.format("%54s", "Enter your full name: "));
-            LOGINname = scanner.nextLine();
-            System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-            System.out.print("\n" + String.format("%54s", "Enter your password: "));
-            LOGINpass = scanner.nextLine();
-            System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-            for (UserDC user : usersD) {
-                if (user.name.equals(LOGINname)) {
+            boolean USERHANDLING = false, PASSHANDLING = false;
+            System.out.println("\n[LOGIN]");
+            System.out.print("\nEnter your full name: ");
+            LOGINname = input.nextLine();
+            System.out.print("\nEnter your password: ");
+            LOGINpass = input.nextLine();
+            for (usersDC users : usersD) {
+                if (users.name.equalsIgnoreCase(LOGINname)) {
                     USERHANDLING = true;
-                    if (user.passw.equals(LOGINpass)) {
-                        PASSHANDLING = true;
-                        System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-                        System.out.println("\n" + String.format("%72s", "[  LOG - IN SUCCESSFUL  ]"));
-                        System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-                        REGISname = user.name;
-                        HOMESCR();
-                        return;
-                    }
-                    break;
+                if (users.pass.equalsIgnoreCase(LOGINpass)) {
+                    PASSHANDLING = true;
+                    System.out.println("\n[LOGIN SUCCESSFUL]");
+                    REGISname = users.name;
+                    HOMESCR();
+                    return;
+                } break;
+              }
+            } if (!USERHANDLING) {
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("\n[USER NOT FOUND]");
                 }
-            }
-
-            if (!USERHANDLING) {
-                System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-                System.out.println("\n" + String.format("%70s", "[  USER NOT FOUND  ]"));
-                System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
             } else if (!PASSHANDLING) {
                 for (int i = 0; i < 3; i++) {
-                    System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-                    System.out.println("\n" + String.format("%71s", "[  INCORRECT INPUT  ]"));
-                    System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
+                    System.out.println("\n[INCORRECT INPUT]");
                 }
             }
         }
     }
 
     public static void HOMESCR() {
-        while (true) {
-            int choice;
-            System.out.println("\n\n" + String.format("%82s", "============================================"));
-            System.out.println(String.format("%73s", "[  WELCOME  " + REGISname + " ]"));
-            System.out.println(String.format("%82s", "============================================"));
-            System.out.println("\n" + String.format("%48s", "1. Return"));
-            System.out.println(String.format("%88s", "--------------------------------------------------------"));
-            System.out.print("\n\n" + String.format("%39s", "Enter: "));
-            try {
-                choice = Integer.parseInt(scanner.nextLine());
-            } catch (NumberFormatException e) {
-                choice = -1;
-            }
-            if (choice == 1) {
-                return;
-            } else {
-                System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-                System.out.println("\n" + String.format("%76s", "[  PLEASE ENTER A CORRECT INPUT  ]"));
-                System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
-            }
-        }
+        System.out.println("\n[WELCOME " + REGISname + " ]");
+        System.out.println("\nPLEASE CLICK ENTER TO GO BACK");
+        input.nextLine();
     }
 
-    public static void EXIT() {
-        System.out.println("\n\n" + String.format("%88s", "--------------------------------------------------------"));
-        System.out.println("\n" + String.format("%67s", "[  THANK YOU  ]"));
-        System.out.println("\n" + String.format("%88s", "--------------------------------------------------------"));
+    public static void exit() {
+        System.out.println("\n[THANK YOU FOR USING!]");
     }
 
     public static void SAVEDFILES() {
-        try (FileWriter file = new FileWriter("users.txt")) {
-            for (UserDC user : usersD) {
-                file.write("NAME: " + user.name + "\nPASSWORD: " + user.passw + "\n");
+        try (PrintWriter receiver = new PrintWriter("users.txt")) {
+            for (usersDC users : usersD) {
+                receiver.println("NAME: " + users.name);
+                receiver.println("PASSWORD: " + users.pass);
             }
         } catch (IOException e) {
-            System.out.println("Error saving file.");
+            for (int i = 0; i < 3; i++) {
+                System.out.println("\n[ERROR]");
+            }
         }
     }
 }
